45fc636c064be0e25a3d50d5a1a909d713295bba
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2022-06-09 19:25:01 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'haushaltsbuch',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
